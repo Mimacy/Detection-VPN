696db8cdd3b3f11e7ae4b8fe684b6dff87d39389
Mimacy <42425387+Mimacy@users.noreply.github.com>
2020-01-21 14:07:14 +0100

    Update detect-vpn-proxy.mind-media.php

--- detect-vpn-proxy.mind-media.php
+++ detect-vpn-proxy.mind-media.php
@@ -1,20 +1,46 @@
 <?php
 
-$ip = gethostbyname($_SERVER["REMOTE_ADDR"]);
- $ch = curl_init();
- curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
- curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
- curl_setopt($ch, CURLOPT_URL, 'http://proxy.mind-media.com/block/proxycheck.php?ip='.$ip);
- $result = curl_exec($ch);
- curl_close($ch);
+function getBooleanFAI($ip) {
+ $host = @gethostbyaddr($ip);
+ $fai = false;
+ if(substr_count($host, 'proxad')) $fai = true;
+ if(substr_count($host, 'orange')) $fai = true;
+ if(substr_count($host, 'wanadoo')) $fai = true;
+ if(substr_count($host, 'sfr')) $fai = true;
+ if(substr_count($host, 'club-internet')) $fai = true;
+ if(substr_count($host, 'neuf')) $fai = true;
+ if(substr_count($host, 'gaoland')) $fai = true;
+ if(substr_count($host, 'bbox')) $fai = true;
+ if(substr_count($host, 'bouyg')) $fai = true;
+ if(substr_count($host, 'numericable')) $fai = true;
+ if(substr_count($host, 'tele2')) $fai = true;
+ if(substr_count($host, 'videotron')) $fai = true;
+ if(substr_count($host, 'belgacom')) $fai = true;
+ if(substr_count($host, 'bell.ca')) $fai = true;
+ return $fai;
+}
 
-if($result == 'N'){
- echo "IP résidentielle / non classifiée (c'est-à-dire Safe IP)";
-}
-else if($result == 'Y'){
- echo "Détection d'un VPN ou PROXY ou VPS ou Serveur dédié ou hébergeur hosting...";
-}
-else {
- echo "Connexion inconnue";
+
+// ignore the French Internet Providers
+if ( getBooleanFAI($_SERVER["REMOTE_ADDR"]) == false ) {
+
+	$ip = gethostbyname($_SERVER["REMOTE_ADDR"]);
+	 $ch = curl_init();
+	 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	 curl_setopt($ch, CURLOPT_URL, 'https://blackbox.ipinfo.app/lookup/'.$ip);
+	 $result = curl_exec($ch);
+	 curl_close($ch);
+
+	if($result == 'N'){
+	 echo "IP rÃ©sidentielle / non classifiÃ©e (c'est-Ã -dire Safe IP)";
+	}
+	else if($result == 'Y'){
+	 echo "DÃ©tection d'un VPN ou PROXY ou VPS ou Serveur dÃ©diÃ© ou hÃ©bergeur hosting...";
+	}
+	else {
+	 echo "Connexion inconnue";
+	}
+
 }
 ?>
